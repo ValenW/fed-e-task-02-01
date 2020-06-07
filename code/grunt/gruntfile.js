0c8269b407590524e3ebd6c3f0a65fb387584af6
Valen <wang_wenl@worksap.co.jp>
2020-06-07 15:29:11 +0800

    add swig in grunt

--- code/grunt/gruntfile.js
+++ code/grunt/gruntfile.js
@@ -1,8 +1,61 @@
 const loadGruntTasks = require('load-grunt-tasks')
 const sass = require('sass')
 
+const data = {
+  menus: [
+    {
+      name: 'Home',
+      icon: 'aperture',
+      link: 'index.html'
+    },
+    {
+      name: 'Features',
+      link: 'features.html'
+    },
+    {
+      name: 'About',
+      link: 'about.html'
+    },
+    {
+      name: 'Contact',
+      link: '#',
+      children: [
+        {
+          name: 'Twitter',
+          link: 'https://twitter.com/w_zce'
+        },
+        {
+          name: 'About',
+          link: 'https://weibo.com/zceme'
+        },
+        {
+          name: 'divider'
+        },
+        {
+          name: 'About',
+          link: 'https://github.com/zce'
+        }
+      ]
+    }
+  ],
+  pkg: require('./package.json'),
+  date: new Date()
+}
+
 module.exports = grunt => {
   grunt.initConfig({
+    swig: {
+      options: {
+        data,
+        cache: false,
+      },
+      main: {
+        files: {
+          'dist/index.html': 'src/index.html',
+          'dist/about.html': 'src/about.html',
+        }
+      }
+    },
     sass: {
       options: {
         sourceMap: true,
